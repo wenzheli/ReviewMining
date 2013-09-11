955a3160ab6c9f516b1eef3ace6abcabae291786
wenzheli <nadalwz1115@hotmail.com>
2013-09-11 09:59:36 -0700

    initial commit

--- src/ml/topicModel/lda/Documents.java
+++ src/ml/topicModel/lda/Documents.java
@@ -1,5 +1,5 @@
 package ml.topicModel.lda;
 
 public class Documents {
-
+    
 }
--- src/ml/topicModel/lda/LDAModel.java
+++ src/ml/topicModel/lda/LDAModel.java
@@ -2,4 +2,38 @@ package ml.topicModel.lda;
 
 public class LDAModel {
     
+    private int D;  // # of documents in the data set
+    private int V;  // vocabulary size
+    
+    private double [][] theta; // document - topic distributions, size M x K
+    private double [][] phi;   // topic-word distributions, size K x V
+    
+    private int[][] z; // latent variable, topic assignments for each word. D * document.size()
+    
+    private int [][] nWordTopic; // nWordTopic[i][j]: # of instances of word/term i assigned to topic j, size V x K
+    private int [][] nDocTopic; // nDocTopic[i][j]: # of words in document i that assigned to topic j, size D x K
+    private int [] nWordTopicSum; // nWordTopicSum[j]: total number of words assigned to topic j, size K. 
+    private int [] nWordsSum; // nWordsSum[i]: total number of words in document i, size D
+    
+    // temp variables for sampling
+    private double [] p; 
+    
+    
+    public void saveParamtersToFile(){
+        
+    }
+    
+    public void loadParametersFromFile(){
+        
+    }
+    
+    public void printTopWords(){
+        
+    }
+    
+    public void saveTopWords(){
+        
+    }
+    
+    
 }
